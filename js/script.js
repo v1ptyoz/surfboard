3faadca4c579b91463425e685a8f32d9727f7bb1
Khrabrov Aleksandr <v1ptyoz@gmail.com>
2021-07-21 20:11:06 +1000

    Логика для отзывов

--- js/script.js
+++ js/script.js
@@ -17,11 +17,6 @@ document.addEventListener('DOMContentLoaded', function() {
         })
     }
     toggleMenu();
-    // function slider() {
-    //     let slideIndex = 1;
-    //     const slides = document.querySelectorAll('.product');
-    //     let prev = document.querySelector('.shop__arrow-prev');
-    //     let next = document.querySelector('.shop__arrow-next');
 
     function slider() {
         let slideIndex = 1;
@@ -61,43 +56,46 @@ document.addEventListener('DOMContentLoaded', function() {
 
     slider();
 
-    //     function hideActive() {
-    //         pagers.forEach(element => {
-    //             element.classList.remove("pager__link--active");
-    //         });
-    //     }
-    //     let reviewIndex = 1;
-    //     let reviews = document.querySelectorAll('.reviews__item');
-    //     let pagers = document.querySelectorAll('.pager__link');
+    function reviewSlider() {
+        function setActive(element) {
+            element.classList.add("pager__link--active");
+        }
 
-    //     reviews.forEach(element => {
-    //         hide(element);
-    //     })
-    //     showFlex(reviews[reviewIndex - 1]);
-    //     setActive(pagers[reviewIndex - 1]);
+        function hideAllActive() {
+            pagers.forEach(element => {
+                element.classList.remove("pager__link--active");
+            });
+        }
+        let reviewIndex = 0;
+        let reviews = document.querySelectorAll('.reviews__item');
+        let pagers = document.querySelectorAll('.pager__link');
 
-    //     pagers.forEach((element, key) => {
-    //         element.addEventListener('click', (event) => {
-    //             event.preventDefault();
-    //             reviewIndex = key + 1;
+        reviews.forEach(element => {
+            hide(element);
+        })
+        showFlex(reviews[reviewIndex]);
+        setActive(pagers[reviewIndex]);
 
-    //             reviews.forEach(element => {
-    //                 hide(element);
-    //             })
-    //             hideActive();
-    //             showFlex(reviews[reviewIndex - 1]);
-    //             setActive(pagers[reviewIndex - 1]);
-    //         })
-    //     });
+        pagers.forEach((element, key) => {
+            element.addEventListener('click', (event) => {
+                event.preventDefault();
+                reviewIndex = key;
 
-    // }
+                reviews.forEach(element => {
+                    hide(element);
+                })
+                hideAllActive();
+                showFlex(reviews[reviewIndex]);
+                setActive(pagers[reviewIndex]);
+            })
+        });
 
-    // reviewSlider();
+    }
+
+    reviewSlider();
 
 
-    // function showGrid(element) {
-    //     element.style.display = 'grid';
-    // }
+
 
     function showFlex(element) {
         element.style.display = 'flex';
