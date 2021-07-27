e2e0c258c7f61a012a585b4097adc43ada57c00d
Khrabrov Aleksandr <v1ptyoz@gmail.com>
2021-07-27 18:56:50 +1000

    Логика для отображения контента в блоке team

--- js/script.js
+++ js/script.js
@@ -61,11 +61,8 @@ document.addEventListener('DOMContentLoaded', function() {
             element.classList.add("pager__link--active");
         }
 
-        function hideAllActive() {
-            pagers.forEach(element => {
-                element.classList.remove("pager__link--active");
-            });
-        }
+
+
         let reviewIndex = 0;
         let reviews = document.querySelectorAll('.reviews__item');
         let pagers = document.querySelectorAll('.pager__link');
@@ -94,6 +91,39 @@ document.addEventListener('DOMContentLoaded', function() {
 
     reviewSlider();
 
+    function teamHandler() {
+        const items = document.querySelectorAll('.team__item');
+        const arrows = document.querySelectorAll('.team__arrow');
+        const itemsContent = document.querySelectorAll('.team__content');
+        itemsContent.forEach(element => element.style.height = 0 + "px");
+        arrows.forEach(element => element.classList.remove('team__arrow--active'));
+
+        items.forEach(item => {
+            const itemArrow = item.querySelector('.team__arrow');
+            const itemContent = item.querySelector('.team__content');
+            item.addEventListener('click', (event) => {
+                event.preventDefault();
+                if (itemContent.classList.contains('team__content--active')) {
+                    itemContent.classList.remove('team__content--active');
+                    itemArrow.classList.remove('team__arrow--active');
+                    itemContent.style.height = 0 + "px";
+
+
+                } else {
+                    itemsContent.forEach(element => {
+                        element.style.height = 0 + "px"
+                        element.classList.remove('team__content--active')
+                    });
+                    arrows.forEach(element => element.classList.remove('team__arrow--active'));
+                    itemArrow.classList.add('team__arrow--active');
+                    itemContent.classList.add('team__content--active');
+                    itemContent.style.height = itemContent.scrollHeight + "px";
+                }
+            })
+        })
+    }
+
+    teamHandler();
 
 
 
